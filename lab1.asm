c79c4d33c13ec64726536b8dee10bbe9c597b513
andruvladimir0v <andruvladimir0v@yandex.ru>
2024-03-09 21:01:17 +0000

    Sdelana_rabotayet_lab1

--- lab1.asm
+++ lab1.asm
@@ -1,27 +1,29 @@
+; Программа, которая предлагает пользователю ввести несколько символов, 
+; сохраняет их в переменных и затем выводит в обратном порядке с 
+; соответствующими надписями.
+
 section .data  ; Секция для данных  
-    input db "Input:", 0xA     ; Переменная db - date.byte (Под каждый символ байт), 0xA - переход на новую строку
+    input db "Input numbers:", 0xA     ; Переменная db - date.byte (Под каждый символ байт), 0xA - переход на новую строку
     inputlen equ $ - input    ;equ - определение длина переменной, inputlen - переменная, $ -текущее место в памяти (H - текущее место в паямти) отсюда и длина
-    fnum db "N1:",  
+    fnum db "Number1: ",  
     fnumlen equ $ - fnum    
-    snum db "N2:",  
+    snum db "Number2: ",  
     snumlen equ $ - snum   
-    tnum db "N3:",  
-    tnumlen equ $ - tnum   
-    output db "Output", 0xA   ; Переменная db - date.byte (Под каждый символ байт), 0xA - переход на новую строку
+    tnum db "Number3: ",  
+    tnumlen equ $ - tnum
+    output db "Numbers in reverse order: ", 0xA   ; Переменная db - date.byte (Под каждый символ байт), 0xA - переход на новую строку
     outputlen equ $ - output  ;equ - определение длина переменной, inputlen - переменная, $ -текущее место в памяти (H - текущее место в паямти) отсюда и длина
    
-
-
 section .bss ; Секция объявления переменных ( 5 - размерность в байтах)
-    num1 resb 5
-    num2 resb 5
-    num3 resb 5
+    number1 resb 5
+    number2 resb 5
+    number3 resb 5
 
 section .text ; Cекция кода 
     global _start       ; Объявление global_start указывает ядру, с чего начинается выполнение программы.
 
 _start:
-;Вывод в консоль "Input:"
+;Вывод в консоль "Input numbers:"
     mov eax, 4          ; 4 - номер системного вызова (sys_write)
     mov ebx, 1          ; 1 - файловый дескриптор (stdout) |  Ко всем потокам ввода-вывода можно получить доступ через так называемые файловые дескрипторы.
     mov ecx, input      ; Переносим переменную input в ecx
@@ -29,59 +31,47 @@ _start:
     int 0x80            ; Вызов ядра
 ;  num1
     
-    ; Вывод в консоль "N1:"
+    ; Вывод в консоль "Number1:"
     mov eax, 4          ; 4 - номер системного вызова (sys_write)
     mov ebx, 1          ; 1 - файловый дескриптор (stdout) |  Ко всем потокам ввода-вывода можно получить доступ через так называемые файловые дескрипторы.
-    mov ecx, fnum      ; Переносим переменную input в ecx
-    mov edx, fnumlen   ; Переносим длину сообщения для вывода в edx
+    mov ecx, fnum       ; Переносим переменную input в ecx
+    mov edx, fnumlen    ; Переносим длину сообщения для вывода в edx
     int 0x80            ; Вызов ядра
     
-
     ; Чтение num1
     mov eax, 3          ; 3 - номер системного вызова  (sys_read)
     mov ebx, 2          ; 2 - файловый дескриптор (stdin)
-    mov ecx, num1       ; Ссылка на переменную в которую запишем число 
+    mov ecx, number1       ; Ссылка на переменную в которую запишем число 
     mov edx, 5          ; 5 - длина сообщения 
-    int 0x80            ; call kernel
+    int 0x80            ; Вызов ядра
 
-
-    
 ;  num2
-    ; Вывод в консоль "N2:"
     mov eax, 4          
     mov ebx, 1         
     mov ecx, snum       
     mov edx, snumlen    
     int 0x80            
     
-    ; Чтение num2
-    mov eax, 3          ; system call number (sys_read)
-    mov ebx, 2          ; file descriptor (stdin)
-    mov ecx, num2       ; address to write
-    mov edx, 5          ; message length
-    int 0x80            ; call kernel
-
-
+    mov eax, 3          
+    mov ebx, 2          
+    mov ecx, number2       
+    mov edx, 5          
+    int 0x80            
     
 ; num3
-    ; Вывод в консоль "N3:"
     mov eax, 4          
     mov ebx, 1          
     mov ecx, tnum      
     mov edx, tnumlen   
     int 0x80            
     
-  
-
-    ; Чтение num3
     mov eax, 3          
     mov ebx, 2          
-    mov ecx, num3       
+    mov ecx, number3       
     mov edx, 5          
     int 0x80            
 
-
-; Вывод "Output:"
+; Вывод "Numbers in reverse order:"
     mov eax, 4
     mov ebx, 1
     mov ecx, output
@@ -91,21 +81,21 @@ _start:
     ; Вывод num3 
     mov eax, 4
     mov ebx, 1
-    mov ecx, num3
+    mov ecx, number3
     mov edx, 5
     int 0x80
 
     ; Вывод num2 
     mov eax, 4
     mov ebx, 1
-    mov ecx, num2
+    mov ecx, number2
     mov edx, 5
     int 0x80
 
     ; Вывод num1 
     mov eax, 4
     mov ebx, 1
-    mov ecx, num1
+    mov ecx, number1
     mov edx, 5
     int 0x80
 
